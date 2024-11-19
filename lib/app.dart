74c9e22fdc396083db2715c5d82d9aaf3c10dda0
AdrianKnapp <adrknapp@gmail.com>
2024-11-19 18:47:08 -0300

    feat(home): added initial code

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 

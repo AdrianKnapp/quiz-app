397a4a2764b67b192779ed18ef0a57041ea9b5ba
AdrianKnapp <adrknapp@gmail.com>
2025-01-03 22:13:32 -0300

    feat(screens): added switch screen method

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,7 +12,13 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
 
   void switchScreen() {
     setState(() {
@@ -33,7 +39,7 @@ class _QuizState extends State<Quiz> {
               ],
             ),
           ),
-          child: StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
